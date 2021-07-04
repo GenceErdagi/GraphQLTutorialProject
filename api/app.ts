5fc74aa4986078b466a1341ea617b9ef2d52ebba
GenceErdagi <huseyingence.erdagi2018@pru.edu.tr>
2021-07-05 00:02:14 +0300

    Adding Lodash

--- api/app.ts
+++ api/app.ts
@@ -1,13 +1,12 @@
-import express from "express";
-import graphqlHTTP from "express-graphql";
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema/schema';
 
 const app = express();
 
 const PORT = 8000;
 
-app.use("/qraphql",graphqlHTTP({
-    
-}))
+app.use('/qraphql', graphqlHTTP({}));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
